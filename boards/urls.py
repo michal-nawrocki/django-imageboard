5a7c77c30dfd935f9563720aeb972ea7be347323
Mike Nawrocki <michal.m.nawrocki@gmail.com>
2020-05-24 18:43:14 +0100

    Allow and display file uploads in threads and posts
    Note: All uploads are stored as file so it will need to improve
    so we only accept displayable media, like images or video.

--- boards/urls.py
+++ boards/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -6,4 +8,4 @@ app_name = "boards"
 urlpatterns = [
     path("", views.index, name="index"),
     path("thread/<int:post_id>/", views.thread, name="thread")
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
