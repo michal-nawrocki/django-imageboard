cb564189bea338405eb08b25afb861300c8e3f5b
Mike Nawrocki <michal.m.nawrocki@gmail.com>
2020-05-24 01:22:50 +0100

    Add Thread view and update urlresolve

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
 
+from .models import Thread, Post
+
 
 def index(request):
-    return HttpResponse("Hello, world. This is the index for all boards.")
+    latest_threads = Thread.objects.order_by("-creation_datetime")[:5]
+    output = ', '.join([t.text for t in latest_threads])
+    return HttpResponse(output)
+
+
+def thread(request, post_id):
+    response = f"You're looking at thread with id:{post_id}"
+    return response
+
--- django_imageboard/urls.py
+++ django_imageboard/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("boards/", include("boards.urls")),
+    path("", include("boards.urls")),
     path('admin/', admin.site.urls),
 ]
